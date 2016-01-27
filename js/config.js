0487b39381547770b3adba33fb60000f89962efb
iansun <lauer3912@gmail.com>
2016-01-26 21:55:43 -0800

    主页，第一次提交

---
+++ js/config.js
@@ -0,0 +1,12 @@
+(function () {
+    window.RTYConfig = {
+        appName:"XLS2csv",
+        appID:"com.romanysoft.app.macos.XLS2csv",
+        documentTitle:"DataStorm By Romanysoft",
+        googleUA:"UA-43238484-8",
+        gitHome:"https://github.com/Romanysoft/XLS2csv",
+        reportIssueUrl:"https://github.com/Romanysoft/XLS2csv/issues",
+        romanysoftHome:"http://www.romanysoft.com",
+        copyright:"Copyright © 2016 Romanysoft LAB."
+    }
+})();
