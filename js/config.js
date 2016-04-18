d43a0b43a811573b095ee3afac147a21e2fd7620
iansun <lauer3912@gmail.com>
2016-04-18 19:04:07 +0800

    更新网站

--- js/config.js
+++ js/config.js
@@ -3,10 +3,13 @@
         appName:"XLS2csv",
         appID:"com.romanysoft.app.macos.XLS2csv",
         documentTitle:"XLS2csv By Romanysoft",
+        supportPlatforms:["Mac"],
         googleUA:"UA-43238484-8",
         gitHome:"https://github.com/Romanysoft/XLS2csv",
         reportIssueUrl:"https://github.com/Romanysoft/XLS2csv/issues",
+        changeLogUrl:"https://github.com/Romanysoft/XLS2csv/wiki/Changelog",
+        wikiUrl:"https://github.com/Romanysoft/XLS2csv/wiki",
         romanysoftHome:"http://www.romanysoft.com",
-        copyright:"Copyright © 2016 Romanysoft LAB."
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
 })();
